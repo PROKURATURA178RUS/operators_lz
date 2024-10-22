7aec777552f20762317d27c5cae311de2a115fd6
PROKURATURA178RUS <priora05tonirovka@tutamail.com>
2024-10-22 18:44:16 +0300

    The third commit

--- simple/simple.py
+++ simple/simple.py
@@ -1,18 +1,17 @@
 input_data = open('simple\input.txt','r') # Создадим файл со входными данными
+output = open('simple\output.txt', 'w') # Откроем файл в который будем выводить ответ
 data = input_data.read ()
-n = int(data[0])
-b = 0
-output_data = open('simple\output.txt','a')
-for a in range(0, 26):
-    if (a%n) == 0  :
-        b = ('Y')
-    elif (a%n) == :
-         b = ('Y')
-    else:
-            b = ('N')
+data = data.split()
+a = int(data[0]) # Присвоим a значение из input
 
+for i in range(2, 26): # Создадим переменную i на диапазаоне от 1 до 25
+    if a%i == 0 and i !=a: # Задаим условия
+        output.write(str('N')) # Вывод N
+        break
+    elif i == a:
+        output.write(str('Y')) # Вывод Y
+    if a == 1:
+        output.write(str('N')) # Вывод N
 
-
-    output_data.write(str(b) + ' ' )
 input_data.close() # Закроем файл
-output_data.close() # Закроем файл       
+output.close() # Закроем файл       
